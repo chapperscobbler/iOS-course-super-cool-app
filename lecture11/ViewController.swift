ae5f50a34d130d03b0746bf8da2cac91a8162ec1
Neil Chapman <chapperscobb@MacBook-Pro.local>
2015-09-29 23:04:24 +0100

    added files from lecture 11

--- lecture11/ViewController.swift
+++ lecture11/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blue_image: UIImageView!
+
+    @IBOutlet weak var bluebutton: UIButton!
+    
+    @IBOutlet weak var redbutton: UIButton!
+    
+    @IBOutlet weak var red_image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PressBlue (sender: AnyObject) {
+        blue_image.hidden = true
+    }
+    
+    @IBAction func PressRed(sender: AnyObject) {
+        red_image.hidden = true
+    }
 
 }
 
